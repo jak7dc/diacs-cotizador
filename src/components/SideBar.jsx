f86543c3e192fcf16544100ac3997baf429f9f6c
yevgeny sierra <diacs.ingenieria@gmail.com>
2024-09-02 08:19:12 -0500

    start 01

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,17 @@
 import '../styles/navBar.css'
+import { Link } from 'react-router-dom'
 
 export const SideBar = () => {
   return (
     <nav className='navleft'>
       <ul className='ul-navleft'>
-        <li>imagen</li>
+        <li>
+          <p>lista de items de inventario</p>
+          <ul>
+            <li><Link>items de cobro</Link></li>
+            <li><Link to='/categories'>Categorias</Link></li>
+          </ul>
+        </li>
         <li>diacs</li>
         <li>logout</li>
       </ul>
